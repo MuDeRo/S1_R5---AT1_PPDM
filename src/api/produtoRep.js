cb1c50065777fb3a35b73adfb9c538c1861a5d3e
MURILO DEMORI RODRIGUES <murilo.rodrigues35@senaisp.edu.br>
2026-06-08 14:55:05 -0300

    feat: adição de registro de produtos com imagens

--- src/api/produtoRep.js
+++ src/api/produtoRep.js
@@ -21,23 +21,23 @@ export const produtoRep = {
 
         try {
 
-            const formData = new FormData();
+            const formData = new FormData(); //Cria um objeto FormData para enviar os dados do produto e a imagem - igual no insomnia
 
             formData.append('idCategoria', String(categoriaId));
             formData.append('nome', nomeProduto);
             formData.append('valor', String(valorProduto));
 
-            formData.append('image', {
+            formData.append('image', { //A chave 'image' deve corresponder ao nome esperado pelo backend (uploadImage) para o arquivo
                 uri: imagem.uri,
                 name: imagem.fileName || 'produto.jpg',
                 type: imagem.mimeType || 'image/jpeg'
             });
 
-            const response = await api.post(
+            const response = await api.post( //faz a requisição POST para criar o produto, enviando o formData com os dados e a imagem
                 '/produtos',
                 formData,
                 {
-                    headers: {
+                    headers: { //Define o cabeçalho para indicar que os dados estão sendo enviados como multipart/form-data
                         'Content-Type': 'multipart/form-data'
                     }
                 }
